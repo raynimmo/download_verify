d38e263fd0d24d20809fabd42f1ed1a6284c7847
Ray Nimmo <ray@junglecreative.com>
2013-08-06 17:11:03 +0700

    Further additions to the module admin panel. Establishing database connection with frontend functionality.

--- js/download_verify.js
+++ js/download_verify.js
@@ -27,12 +27,13 @@
 
   // Main form content containing fields for firstname, secondname, email address and a submit button
   // Form contains development element span.filepath-display - remove from production
-  /* TODO : make it better!
-      options: - construct the form using DOM methods
+  /* TODO : clean this up!
+      options: - construct the form using DOM methods?
                - can Webform help?
+               - prototype elements?
   */
   Drupal.theme.prototype.download_verify_form_wrapper = function() {
-    return '<div><div class="form-item form-type-textfield form-item-fname"><label for="edit-fname">Firstname <span title="This field is required." class="form-required">*</span></label><input type="text" class="form-text required" maxlength="60" size="20" value="" name="fname" id="edit-fname"></div><div class="form-item form-type-textfield form-item-name"><label for="edit-sname">Surname <span title="This field is required." class="form-required">*</span></label><input type="text" class="form-text required" maxlength="60" size="20" value="" name="sname" id="edit-sname"></div><div class="form-item form-type-textfield form-item-name"><label for="edit-email">Email <span title="This field is required." class="form-required">*</span></label><input type="text" class="form-text required" maxlength="60" size="20" value="" name="email" id="edit-email"></div><div id="edit-actions" class="form-actions form-wrapper"><a class="button form-submit" id="edit-submit" onclick="validateForm();">Download</a><a class="button form-cancel" id="edit-cancel"  onclick="verifyClose();">Cancel</a></div></div><p class="msgbox"><b>Selected Download:</b><br /><span class="filename-display"></span><span class="filepath-display"></span></p>';
+    return '<div><div class="form-item form-type-textfield form-item-fname"><label for="edit-fname">First name <span title="This field is required." class="form-required">*</span></label><input type="text" class="form-text required" maxlength="60" size="20" value="" name="fname" id="edit-fname"></div><div class="form-item form-type-textfield form-item-name"><label for="edit-sname">Surname <span title="This field is required." class="form-required">*</span></label><input type="text" class="form-text required" maxlength="60" size="20" value="" name="sname" id="edit-sname"></div><div class="form-item form-type-textfield form-item-name"><label for="edit-email">Email <span title="This field is required." class="form-required">*</span></label><input type="text" class="form-text required" maxlength="60" size="20" value="" name="email" id="edit-email"></div><div id="edit-actions" class="form-actions form-wrapper"><a class="button form-submit" id="edit-submit" onclick="validateForm();">Download</a><a class="button form-cancel" id="edit-cancel"  onclick="verifyClose();">Cancel</a></div></div><p class="msgbox"><b>Selected Download:</b><br /><span class="filename-display"></span><span class="filepath-display"></span></p>';
 
   }
 
@@ -58,7 +59,7 @@
       var filepath;
 
       //hijack the pdf clicks
-      $('.' + download_verify_css_target +' a').click(function(event){
+      $('.' + download_verify_css_target +' a', context).click(function(event){
 
         //TODO: check for cookie on users machine
 
